a368d85a302bc878bb806b4054fccd17d1530747
ejrach <eric.rach@gmail.com>
2019-10-26 20:09:25 -0500

    Added an AddTvShowWindow

---
+++ SeasonTracker/View/AddTvShowWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SeasonTracker.View
+{
+    /// <summary>
+    /// Interaction logic for AddTvShowWindow.xaml
+    /// </summary>
+    public partial class AddTvShowWindow : Window
+    {
+        public AddTvShowWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SeasonTracker/ViewModel/Commands/AddTvShowCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;     //needed for ICommand interface for button pressing
+
+namespace SeasonTracker.ViewModel.Commands
+{
+    class AddTvShowCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
