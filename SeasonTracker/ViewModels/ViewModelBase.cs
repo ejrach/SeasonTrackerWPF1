c0a829f6ac5e290eb7892680e83fed9875e2226c
ejrach <eric.rach@gmail.com>
2019-10-27 18:20:59 -0600

    Implemented ICommand interface for MyList button

--- SeasonTracker/ViewModels/ViewModelBase.cs
+++ SeasonTracker/ViewModels/ViewModelBase.cs
@@ -10,11 +10,13 @@ namespace SeasonTracker.ViewModels
     public class ViewModelBase
     {
         public AddTvShowCommand AddTvShowCommand { get; set; }
+        public MyListCommand MyListCommand { get; set; }
 
         //Constructor - initialize all commands here...
         public ViewModelBase()
         {
             this.AddTvShowCommand = new AddTvShowCommand(this);
+            this.MyListCommand = new MyListCommand(this);
         }
 
         //Method to be executed when the "Add Tv Show" button is clicked.
@@ -22,6 +24,12 @@ namespace SeasonTracker.ViewModels
         {
             AddTvShowWindow addTvShowWindow = new AddTvShowWindow();
             addTvShowWindow.ShowDialog();
+        }
+
+        //Method to be executed when the "My List" button is clicked.
+        public void MyList()
+        {
+            //TBD Add method actions here...
         }
     }
 }
